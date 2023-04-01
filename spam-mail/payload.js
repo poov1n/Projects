1d56a32465e52b494f46e5e80a40db891618eaa4
Poovin <108090982+V4g4b0nd@users.noreply.github.com>
2023-04-01 12:15:38 +0530

    Update payload.js

--- spam-mail/payload.js
+++ spam-mail/payload.js
@@ -1,3 +1,5 @@
+//payload that we use in the html pages, bear in mind to change the input type based on the webpages 
+
 <script src ="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script>
 	
